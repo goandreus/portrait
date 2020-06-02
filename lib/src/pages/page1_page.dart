a637f9af450bbc355c91416e605324a5c03fd171
goandreus <andreshtml5@gmail.com>
2020-06-02 02:21:58 -0500

    elastic animated

--- lib/src/pages/page1_page.dart
+++ lib/src/pages/page1_page.dart
@@ -1,7 +1,7 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-
 
 class Page1Page extends StatelessWidget {
   const Page1Page({Key key}) : super(key: key);
@@ -10,34 +10,61 @@ class Page1Page extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Animed'),
+        title: FadeIn(
+          duration: Duration(milliseconds: 500),
+          child: Text('Animate')),
         actions: <Widget>[
           IconButton(
             icon: FaIcon(FontAwesomeIcons.twitter),
-            onPressed: (){},
+            onPressed: () {},
           ),
-          IconButton(
-            icon: Icon(Icons.navigate_next),
-            onPressed: (){},  
+          SlideInLeft(
+            from: 10,
+            child: IconButton(
+              icon: Icon(Icons.navigate_next),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                        builder: (BuildContext context) => Page1Page()));
+              },
+            ),
           ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: FaIcon(FontAwesomeIcons.play),
-        onPressed: (){},
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          child: FaIcon(FontAwesomeIcons.play),
+          onPressed: () {},
+        ),
       ),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Icon(Icons.new_releases, color: Colors.blue, size:40),
-            Text('Title', style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200 ),),
-            Text('I am child text', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),),
-            Container(
-              width: 220,
-              height: 2,
-              color: Colors.blue,
+            ElasticIn(
+                duration: Duration(milliseconds: 2000),
+                child: Icon(Icons.new_releases, color: Colors.blue, size: 40)),
+            FadeInDown(
+                delay: Duration(milliseconds: 2000),
+                child: Text(
+                  'Title',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
+                )),
+            FadeInDown(
+              duration: Duration(milliseconds: 3000),
+              child: Text(
+                'I am child text',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+              ),
+            ),
+            FadeInLeft(
+              delay: Duration(milliseconds: 1100),
+              child: Container(
+                width: 220,
+                height: 2,
+                color: Colors.blue,
+              ),
             )
           ],
         ),
